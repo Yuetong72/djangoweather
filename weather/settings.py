769d89f04f286302774f23fc414d87fc07f9b6e8
Yuetong Yang <ytyang828@gmail.com>
2020-12-17 16:02:59 -0600

    fix some bugs

--- weather/settings.py
+++ weather/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'pejb84dax@s!k##+tph4anyf(ko!yx5-^vsqk1rw=iarw*^$&v'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
